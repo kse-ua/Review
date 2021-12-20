26a601b4ae356534e5c83eb7b502a6a093e04dc1
Andrew Trybushnyi <94055869+roflmyrlok@users.noreply.github.com>
2021-12-20 14:14:13 +0200

    array usage instead of str
    now key is array with x empty elements, and each element is replaced by random character from characters array

--- Tasks/password-1.js
+++ Tasks/password-1.js
@@ -1,17 +1,20 @@
 // Generate random password
 
-let GeneratePassword = ( length ) => {
-  const MAX = characters.length
-  key = '';
-  for ( let i = 0; i < length; i++ ){
-    Index = Math.floor(Math.random() * MAX);
-    key = key + characters[Index];
-  };
+let GeneratePassword = (length_of_password) => {
+  const max = characters.length
+  key = [];
+  key.length = length_of_password;
+  for ( let i = 0; i < length_of_password; i++ ){
+    Index = Math.floor(Math.random() * max);
+    key[i] = characters[Index];
+    };
   return key
 }
-const characters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-                    'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D',
-                    'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+
+const characters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e',
+                    'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+                    'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+                    'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
                     'Y', 'Z', '!', '@', '#', '$', '%', '^', '&', '*'];
-const result = GeneratePassword(7);
-console.log(result);
+const result = GeneratePassword(8);
+console.log(result.join(''));
