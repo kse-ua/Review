ab956cd3aba9443c26fe53adfcece0c763477041
Andrew Trybushnyi <94055869+roflmyrlok@users.noreply.github.com>
2021-12-20 14:10:31 +0200

    list of allowed elements
    using list of permitted elements for passwords instead of sending allowed characters

--- Tasks/password-1.js
+++ Tasks/password-1.js
@@ -1,14 +1,17 @@
 // Generate random password
 
-let GeneratePassword = ( alphabet , length ) => {
-  const MAX = alphabet.length
+let GeneratePassword = ( length ) => {
+  const MAX = characters.length
   key = '';
   for ( let i = 0; i < length; i++ ){
     Index = Math.floor(Math.random() * MAX);
-    key = key + alphabet[Index];
+    key = key + characters[Index];
   };
   return key
 }
-
-const result = GeneratePassword('abc123', 7);
+const characters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+                    'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D',
+                    'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+                    'Y', 'Z', '!', '@', '#', '$', '%', '^', '&', '*'];
+const result = GeneratePassword(7);
 console.log(result);
