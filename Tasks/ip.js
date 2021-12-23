e14cf39ac3921e68e11ec6485f6ec7a6f0c35473
Yaroslav Melnichuk <94102798+LieTieOsu@users.noreply.github.com>
2021-12-23 21:34:07 +0200

    Update ip.js

--- Tasks/ip.js
+++ Tasks/ip.js
@@ -1,17 +1,11 @@
 // Split string by the first occurrence of separator
 
 Parseip = i => {
-  a = []
-    if (i === '' ) return 0  ;
-  else {
-  B = i.split('.');
-  j = 0
-    for ( const b of B) {
-  a[j] = parseInt(b);
-  j++;
-  }};
-  return a;
-};
-
-const result = Parseip('127.0.0.1')
-console.log(result)
+    a = []
+    if (i == '' ) return 0 //Если строка с ip пустая, возвращаем 0
+    B = i.split('.') //Делим строку с ip на отдельные цифры
+    if (B.length != 4) return 0 //Отсеиваем ненастоящие/неполные ip
+    B.forEach(b => a.push(parseInt(b))) //Запускаем цикл foreach (Асинхронный цикл) | Добавляем каждый элемент в выходной массив
+    return a; //Возвращаем массив с числами
+}
+console.log(Parseip('127.0.0.1')) //Выводим в выход функции в консоль
